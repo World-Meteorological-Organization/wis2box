b29fb49548571c85df23e27b998b7fb97186b6ae
alhelguera <alhelguera98@gmail.com>
2025-03-31 15:26:19 -0300

    reverting test_workflow and changing tests-docker

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -31,9 +31,6 @@ from pathlib import Path
 from pywis_pubsub.validation import validate_message
 from requests import Session, codes
 
-import sys
-sys.path.insert(1, '/home/runner/work/wis2box/wis2box/wis2box-management/wis2box/metadata')
-from station import publish_from_csv
 
 DATADIR = Path('.').parent.absolute() / 'tests/data'
 
@@ -125,15 +122,6 @@ def test_metadata_station_publish():
 
     assert stations['numberReturned'] == 104
     assert stations['numberMatched'] == 104
-
-
-def test_metadata_station_encoding():
-    """Test station metadata encoding"""
-
-    try:
-        publish_from_csv(path=DATADIR / "metadata/station/brazil.csv")
-    except Exception as err:
-        assert "Invalid utf-8 in station metadata file" in str(err)
 
 
 def test_metadata_discovery_publish():
