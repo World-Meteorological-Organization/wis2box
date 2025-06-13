99297d3858c0ccb0b2ccce3169632807ef4a36e4
Maaike <maaike.limper@gmail.com>
2025-06-13 13:37:05 +0200

    change error to warning, don't raise RunTimeError

--- wis2box-management/wis2box/metadata/discovery.py
+++ wis2box-management/wis2box/metadata/discovery.py
@@ -333,12 +333,11 @@ def publish_discovery_metadata(metadata: Union[dict, str]):
         ts.raise_for_status()
     except TestSuiteError as err:
         # if the only error is about the WIS2 topic, continue with publishing
-        if len(err.errors) == 1 and 'message' in err.errors[0] and err.errors[0]['message'] == 'Invalid WIS2 topic for Pub/Sub link channel': # noqa
-            LOGGER.warning('Invalid WIS2 topic for Pub/Sub link channel only, continuing with publishing')  # noqa
+        if len(err.errors) == 1 and 'message' in err.errors[0] and err.errors[0]['message'] == 'Invalid WIS2 topic (unknown centre-id)  for Pub/Sub link channel': # noqa
+            LOGGER.warning(f'{err.errors[0]['message']} continuing with publishing')  # noqa
         else:
             msg = 'WCMP2 validation errors: ' + ', '.join([err['message'] for err in err.errors])	 # noqa
-            LOGGER.error(msg)
-            raise RuntimeError(msg)
+            LOGGER.warning(msg)
 
     LOGGER.debug('Saving to object storage')
     data_bytes = json.dumps(record,
