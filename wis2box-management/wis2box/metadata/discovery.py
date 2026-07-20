b0c48ce3264d62cd7c307510849802f952992ee8
Tom Kralidis <tomkralidis@gmail.com>
2026-07-20 07:38:52 -0400

    test

--- wis2box-management/wis2box/metadata/discovery.py
+++ wis2box-management/wis2box/metadata/discovery.py
@@ -219,7 +219,8 @@ def publish_broker_message(record: dict, storage_path: str,
     topic = f'origin/a/wis2/{centre_id.lower()}/metadata'  # noqa
 
     datetime_ = datetime.strptime(record['properties']['created'], '%Y-%m-%dT%H:%M:%SZ') # noqa
-    wis_message = WISNotificationMessage(record['id'],
+    identifier = f"{centre_id.lower()}/metadata/{record['id']}"
+    wis_message = WISNotificationMessage(identifier=identifier,
                                          metadata_id=record['id'],
                                          filepath=storage_path,
                                          datetime_=datetime_,
